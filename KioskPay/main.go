edab913b65f659c7cd76c2df103fdd407d0c2ef6
Juan <70347526+fait-arch@users.noreply.github.com>
2023-11-16 02:21:59 -0500

    Prueba de conexion web
    Incorporacion de carpetas static y templates para la creacion de un sitio web con go

---
+++ KioskPay/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Lee el contenido del archivo HTML
+	htmlContent, err := readFile("static/index.html")
+	if err != nil {
+		http.Error(w, "Error al leer el archivo HTML", http.StatusInternalServerError)
+		return
+	}
+
+	// Escribe el contenido en la respuesta HTTP
+	tmpl, err := template.New("index").Parse(htmlContent)
+	if err != nil {
+		http.Error(w, "Error al analizar el archivo HTML", http.StatusInternalServerError)
+		return
+	}
+
+	tmpl.Execute(w, nil)
+}
+
+func readFile(filename string) (string, error) {
+	// Obtiene la ruta absoluta del archivo
+	filePath, err := filepath.Abs(filename)
+	if err != nil {
+		return "", err
+	}
+
+	// Lee el contenido del archivo
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+
+	return string(content), nil
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("src/static/index.html"))))
+	fmt.Println("Servidor escuchando en http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
