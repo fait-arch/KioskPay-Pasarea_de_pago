cbf5270cf47020ba050c76a1f61dca0352e2604c
Juan <70347526+fait-arch@users.noreply.github.com>
2023-11-21 23:59:39 -0500

    Importacion de datos de forms
    Se traen los datos del forms de HTML a golang y se imprimen en terminal

--- KioskPay/main.go
+++ KioskPay/main.go
@@ -8,15 +8,39 @@ import (
 	"path/filepath"
 )
 
+// CONECTAR AL INDEX
 func handler(w http.ResponseWriter, r *http.Request) {
-	// Lee el contenido del archivo HTML
+	if r.Method == "POST" {
+		// Manejar la solicitud POST
+		err := r.ParseForm()
+		if err != nil {
+			http.Error(w, "Error al analizar los datos del formulario", http.StatusInternalServerError)
+			return
+		}
+
+		// Realiza acciones con los datos, por ejemplo, imprímelos en la terminal
+		fmt.Println("Datos del formulario:")
+		fmt.Println("Nombre Completo:", r.FormValue("fullName"))
+		fmt.Println("Dirección:", r.FormValue("address"))
+		fmt.Println("Ciudad:", r.FormValue("city"))
+		fmt.Println("Reguion:", r.FormValue("state"))
+		fmt.Println("Codigo Postal:", r.FormValue("zipCode"))
+		fmt.Println("Pais:", r.FormValue("country"))
+		fmt.Println("Email:", r.FormValue("email"))
+		fmt.Println("Celular:", r.FormValue("phone"))
+
+		// Responder al cliente (puedes enviar un JSON u otro tipo de respuesta si es necesario)
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	// Manejar la solicitud GET (cargar el formulario)
 	htmlContent, err := readFile("templates/index.html")
 	if err != nil {
 		http.Error(w, "Error al leer el archivo HTML", http.StatusInternalServerError)
 		return
 	}
 
-	// Escribe el contenido en la respuesta HTTP
 	tmpl, err := template.New("index").Parse(htmlContent)
 	if err != nil {
 		http.Error(w, "Error al analizar el archivo HTML", http.StatusInternalServerError)
@@ -30,6 +54,7 @@ func readFile(filename string) (string, error) {
 
 	// Obtiene la ruta absoluta del archivo
 	filePath, err := filepath.Abs(filename)
+	//Mensaje de error
 	if err != nil {
 		return "", err
 	}
@@ -45,11 +70,12 @@ func readFile(filename string) (string, error) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	// Manejador para los archivos estáticos (CSS y JavaScript)
+	// Manejador para los archivos estáticos (CSS,HTML y JavaScript)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.Handle("/templates/img/", http.StripPrefix("/templates/img/", http.FileServer(http.Dir("templates/img"))))
 
 	//Abrir puerto local 8080
 	fmt.Println("Servidor escuchando en http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
+
 }
