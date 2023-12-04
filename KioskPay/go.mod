468de66c1509a54d53e41c4b52d2b5ff3a20e4be
Juan <70347526+fait-arch@users.noreply.github.com>
2023-12-04 00:14:57 -0500

    Incorporacion_Base_Datos
    Actulziacion de redacion del codigo
    
    Co-Authored-By: leogerm <82969979+leogerm@users.noreply.github.com>

--- KioskPay/go.mod
+++ KioskPay/go.mod
@@ -5,16 +5,32 @@ go 1.21.4
 require github.com/gin-gonic/gin v1.9.1
 
 require (
+	cloud.google.com/go v0.111.0 // indirect
+	cloud.google.com/go/compute v1.23.3 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	cloud.google.com/go/firestore v1.14.0 // indirect
+	cloud.google.com/go/iam v1.1.5 // indirect
+	cloud.google.com/go/longrunning v0.5.4 // indirect
+	cloud.google.com/go/storage v1.35.1 // indirect
+	firebase.google.com/go v3.13.0+incompatible // indirect
 	github.com/bytedance/sonic v1.10.2 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20230717121745-296ad89f973d // indirect
 	github.com/chenzhuoyu/iasm v0.9.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-logr/logr v1.3.0 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.16.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/s2a-go v0.1.7 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
+	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
@@ -24,11 +40,25 @@ require (
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/otel v1.21.0 // indirect
+	go.opentelemetry.io/otel/metric v1.21.0 // indirect
+	go.opentelemetry.io/otel/trace v1.21.0 // indirect
 	golang.org/x/arch v0.6.0 // indirect
 	golang.org/x/crypto v0.16.0 // indirect
 	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/oauth2 v0.15.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
+	google.golang.org/api v0.152.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/genproto v0.0.0-20231127180814-3a041ad873d4 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20231127180814-3a041ad873d4 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231127180814-3a041ad873d4 // indirect
+	google.golang.org/grpc v1.59.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- KioskPay/main.go
+++ KioskPay/main.go
@@ -4,18 +4,23 @@ package main
 
 import (
 	//CreateProduct "KioskPay/pkg"
-	CreateUser "KioskPay/pkg/CreateUser"
+	//CreateUser "KioskPay/pkg/CreateUser"
+	"context"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"os/exec"
 
+	firebase "firebase.google.com/go"
 	"github.com/gin-gonic/gin"
+	"google.golang.org/api/option"
 )
 
 func main() {
 
-	//------------------------------
+	//----------POCKET BASE---------
 	//  LEVANTAMIENTO DE POCKET BASE
 	//------------------------------
 	// Ruta al ejecutable
@@ -34,60 +39,96 @@ func main() {
 			fmt.Printf("Error al ejecutar el comando: %v\n", err)
 		}
 	}()
-	/*
-	   //-------------------
-	   //  ENVIO A PRODUCTS
-	   //-------------------
-	   product := &CreateProduct.Product{
-	       ProductImg:        "",
-	       ProductName:       "",
-	       ProductPrice:      "",
-	       ProductDescripion: "",
-	   }
 
-
-	   resp, err := CreateProduct.CreateProduct(product)
-	   if err != nil {
-	       fmt.Println("Error:", err)
-	       return
-	   }
-	   defer resp.Body.Close()
-	   fmt.Println(product)
-	   fmt.Println("Product created successfully")
-	*/
-
-	//--------------------------------------------
-	//  CREACION DE UN SERVIDOR WEB SIMPLE CON GIN
-	//--------------------------------------------
-
+	//--------------------GIN TONIC--------------------
+	//  LEVANTAMIENTO DE UN SERVIDOR WEB SIMPLE CON GIN
+	//-------------------------------------------------
+	//
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
-
 	// Rutas estáticas para servir archivos estáticos y publicos (HTML, CSS y JavaScript)
 	router.Static("/static", "./public_web/static")
 	router.StaticFS("/img", http.Dir("./public_web/templates/img"))
 	router.LoadHTMLGlob("public_web/templates/*.html")
-
 	// Ruta para la página principal
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{})
 	})
 
-	//------------------------------------
-	//  LEER DATOS DE FORMULARIO DE USERS
-	//------------------------------------
-	// Ruta POST para manejar la entrada del formulario
-	router.POST("/", func(c *gin.Context) {
-		CreateUser.CreateUser(c)
-		c.Redirect(http.StatusFound, "/") // Redirige de vuelta a la página principal después de enviar
-	})
+	//-----------------FIREBASE----------------
+	//  LEVANTAMIENTO DE CONECCION CON FIREBASE
+	//-----------------------------------------
+	// Leer el contenido del archivo de credenciales de Firebase
+	data, err := ioutil.ReadFile("KEY.json")
+	if err != nil {
+		log.Fatalf("error al leer la key file: %v\n", err)
+	}
+	// Crear una opción de configuración para la autenticación de Firebase
+	opt := option.WithCredentialsJSON(data)
+	// Inicializar una nueva aplicación Firebase
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		log.Fatalf("error levantar app: %v\n", err)
+	}
+	// Obtener un cliente Firestore a partir de la aplicación Firebase
+	client, err := app.Firestore(context.Background())
+	if err != nil {
+		log.Fatalf("error getting Firestore client: %v\n", err)
+	}
+	defer client.Close() //Asegurar el cierre del cliente al final de la función
 
-	// Configurar la confianza en los proxies
-	router.ForwardedByClientIP = true
+	/*
+			//
+			//  LEER DATOS DE FORMULARIO DE USERS
+			//
+			// Ruta POST para manejar la entrada del formulario
+			router.POST("/", func(c *gin.Context) {
+				CreateUser.CreateUser(c)
+				// Redirige de vuelta a la página principal después de enviar
+				c.Redirect(http.StatusFound, "/")
+			})
+
+			//---------------------------------
+			//  ENVIO A USERS A FIREBASE
+			//---------------------------------
+			// Agregar un documento a la colección "Users" en Firestore
+			_, _, err = client.Collection("Users").Add(context.Background(), map[string]interface{}{
+				"user_Name": "texto_prueba_002",
+			})
+			if err != nil {
+				log.Fatalf("error adding document: %v\n", err)
+			}
+			// Imprimir un mensaje indicando que el documento se ha agregado con éxito
+			log.Println("Document added successfully.")
+
+
+
+		   //---------------------------------
+		   //  ENVIO A PRODUCTS A POCKET BASE
+		   //---------------------------------
+		   product := &CreateProduct.Product{
+		       ProductImg:        "",
+		       ProductName:       "",
+		       ProductPrice:      "",
+		       ProductDescripion: "",
+		   }
+
+
+		   resp, err := CreateProduct.CreateProduct(product)
+		   if err != nil {
+		       fmt.Println("Error:", err)
+		       return
+		   }
+		   defer resp.Body.Close()
+		   fmt.Println(product)
+		   fmt.Println("Product created successfully")
+	*/
 
 	//
 	//INCIAR :8080
 	//
+	// Configurar la confianza en los proxies
+	router.ForwardedByClientIP = true
 	// Habilitar el puerto 8080
 	fmt.Println("🅢 🅔 🅡 🅥 🅔 🅡   🅡 🅤 🅝 🏃‍♂️🏃‍♂️")
 	router.Run(":8080")
